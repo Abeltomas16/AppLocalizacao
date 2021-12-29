e9862f609cfb44865a2b171a24d89f9994336a40
Abel.Tomas.16 <abeljosetoms115@gmail.com>
2021-12-29 04:28:01 +0100

    FEAT: implementada metódo de pedir permissão

---
+++ AppLocalizacao/AppLocalizacao.Android/Services/VerificaPermissaoService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AppLocalizacao.Contratos;
+using AppLocalizacao.Droid.Services;
+using Xamarin.Essentials;
+
+[assembly: Xamarin.Forms.Dependency(typeof(VerificaPermissaoService))]
+namespace AppLocalizacao.Droid.Services
+{
+    public class VerificaPermissaoService : ILocalizacaoVerificar
+    {
+        public async Task VerificaPermissao()
+        {
+            await Permissions.RequestAsync<Permissions.LocationAlways>();
+        }
+    }
+}
